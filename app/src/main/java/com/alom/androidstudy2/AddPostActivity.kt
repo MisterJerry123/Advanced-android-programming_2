40655874ada872c6019c0f65743997ba308f3413
MisterJerry <misterjerry@sju.ac.kr>
2024-11-02 19:11:34 +0900

    feat: Implement move to previous Activity by button

--- app/src/main/java/com/alom/androidstudy2/AddPostActivity.kt
+++ app/src/main/java/com/alom/androidstudy2/AddPostActivity.kt
@@ -1,5 +1,6 @@
 package com.alom.androidstudy2
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.alom.androidstudy2.databinding.ActivityAddPostBinding
@@ -11,7 +12,11 @@ class AddPostActivity:AppCompatActivity() {
         binding = ActivityAddPostBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
-
+        val backIntent = Intent(this, MainActivity::class.java)
+        binding.tvBack.setOnClickListener {
+            startActivity(backIntent)
+            finish()
+        }
     }
 
 
