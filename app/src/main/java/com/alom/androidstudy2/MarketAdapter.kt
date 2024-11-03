eb91b9f51bb71452fe1d259bff8079efb6ffb97b
MisterJerry <misterjerry@sju.ac.kr>
2024-11-03 20:34:43 +0900

    feet: Implement RecyclerView Adapter

--- app/src/main/java/com/alom/androidstudy2/MarketAdapter.kt
+++ app/src/main/java/com/alom/androidstudy2/MarketAdapter.kt
@@ -1,17 +1,30 @@
 package com.alom.androidstudy2
 
+import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.alom.androidstudy2.databinding.ItemMarketBinding
+import com.bumptech.glide.Glide
 
 class MarketAdapter(val postList: ArrayList<PostData>) :
     RecyclerView.Adapter<RecyclerView.ViewHolder>() {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
-        TODO("Not yet implemented")
+        val binding = ItemMarketBinding.inflate(LayoutInflater.from(parent.context),parent,false)
+        return MarketViewHolder(binding)
     }
 
     override fun getItemCount() = postList.size
 
     override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
-        TODO("Not yet implemented")
+        if(holder is MarketViewHolder){
+            Glide
+                .with(holder.itemView.context)
+                .load(postList[position].img)
+                .into(holder.img)
+
+            holder.goodsTime.text = postList[position].time
+            holder.goodsPrice.text = postList[position].price
+            holder.goodsTitle.text = postList[position].title
+        }
     }
 }
