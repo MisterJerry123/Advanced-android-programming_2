bbdbd0b452e3d93113f5e1c33741fa21166d7222
MisterJerry <misterjerry@sju.ac.kr>
2024-11-04 13:42:09 +0900

    refactor: Revise Data Class

--- app/src/main/java/com/alom/androidstudy2/PostData.kt
+++ app/src/main/java/com/alom/androidstudy2/PostData.kt
@@ -1,8 +1,18 @@
 package com.alom.androidstudy2
 
+import androidx.resourceinspection.annotation.Attribute.IntMap
+import com.bumptech.glide.load.ImageHeaderParser.ImageType
+
 data class PostData(
-    val title: String,
-    val price: String,
-    val time: String,
-    val img: String
+    val result:Int,
+    val message:String,
+    val data:ArrayList<Data>
+)
+
+data class Data(
+    val id:Int,
+    val title:String,
+    val price:String,
+    val image_url:String,
+    val time:String
 )
