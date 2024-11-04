8a7a57098b65c4916dc78d0c90f35ab14ea276c1
MisterJerry <misterjerry@sju.ac.kr>
2024-11-04 15:17:09 +0900

    feat: implement RetrofitClient

--- app/src/main/java/com/alom/androidstudy2/RetrofitClient.kt
+++ app/src/main/java/com/alom/androidstudy2/RetrofitClient.kt
@@ -6,30 +6,18 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object RetrofitClient {
-
+    val BASEURL="https://goaplrynweyxovekoezl.supabase.co/rest/v1/"
     val instance : Api by lazy{
-
-            val logging = HttpLoggingInterceptor()
-            logging.setLevel(HttpLoggingInterceptor.Level.BODY)
-
-            val okHttpClient = OkHttpClient.Builder()
-                .addInterceptor(logging)
-                .build()
-
-            val retrofit = Retrofit.Builder()
-                .baseUrl("")
-                .client(okHttpClient)
-                .addConverterFactory(GsonConverterFactory.create())
-                .build()
-
-            retrofit.create(Api::class.java)
-
-
+        val logging = HttpLoggingInterceptor()
+        logging.setLevel(HttpLoggingInterceptor.Level.BODY)
+        val okHttpClient = OkHttpClient.Builder()
+            .addInterceptor(logging)
+            .build()
+        val retrofit = Retrofit.Builder()
+            .baseUrl(BASEURL)
+            .client(okHttpClient)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+        retrofit.create(Api::class.java)
     }
-
-
-
-
-
-
 }
