a7c3637810fa7f9606ac4ec46818c7d2a69a7a8a
MisterJerry <misterjerry@sju.ac.kr>
2024-11-04 15:14:00 +0900

    feat: Implement getItem Api from server

--- app/src/main/java/com/alom/androidstudy2/MainActivity.kt
+++ app/src/main/java/com/alom/androidstudy2/MainActivity.kt
@@ -1,27 +1,52 @@
 package com.alom.androidstudy2
 
-import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.LinearLayout
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.alom.androidstudy2.databinding.ActivityMainBinding
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class MainActivity : AppCompatActivity() {
-    lateinit var binding :ActivityMainBinding
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val addPostIntent = Intent(this,AddPostActivity::class.java)
+        val addPostIntent = Intent(this, AddPostActivity::class.java)
 
         binding.tvAddPost.setOnClickListener {
             startActivity(addPostIntent)
             finish()
         }
+
+
+        RetrofitClient.instance.getItems(api = BuildConfig.apiKey)
+            .enqueue(object : Callback<PostData> {
+                override fun onResponse(call: Call<PostData>, response: Response<PostData>) {
+                    if (response.isSuccessful) {
+                        binding.rcvMarket.adapter = MarketAdapter(response.body()!!.data)
+                        binding.rcvMarket.layoutManager = LinearLayoutManager(
+                            this@MainActivity,
+                            LinearLayoutManager.VERTICAL,
+                            false
+                        )
+                    } else {
+                        Toast.makeText(this@MainActivity, "internet error", Toast.LENGTH_SHORT)
+                            .show()
+                    }
+                }
+
+                override fun onFailure(call: Call<PostData>, t: Throwable) {
+                    Toast.makeText(this@MainActivity, "internet error", Toast.LENGTH_SHORT).show()
+                }
+
+            })
 
 
     }
